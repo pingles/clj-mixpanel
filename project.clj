b84660f0eaf0aa81765da7c98443c9035e9c0426
Paul Ingles <paul@oobaloo.co.uk>
2012-06-07 20:29:58 +0100

    initial stuff works

--- project.clj
+++ project.clj
@@ -1,3 +1,6 @@
 (defproject clj-mixpanel "0.0.1-SNAPSHOT"
   :description "Send events to Mixpanel"
-  :dependencies [[org.clojure/clojure "1.4.1"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [clj-http "0.4.2"]
+                 [org.clojure/data.json "0.1.2"]
+                 [commons-codec/commons-codec "1.6"]])
--- src/clj_mixpanel/core.clj
+++ src/clj_mixpanel/core.clj
@@ -1,1 +1,67 @@
-(ns clj-mixpanel.core)
+(ns clj-mixpanel.core
+  (:import [org.apache.commons.codec.binary Base64]
+           [java.util Date UUID])
+  (:require [clj-http.client :as http]
+            [clojure.string :as s])
+  (:use [clojure.data.json :only (json-str)]))
+
+(def api-url "http://api.mixpanel.com/track/")
+
+(defn generate-uuid
+  "Can be used to generate a unique ID suitable for identifying users."
+  []
+  (s/replace (str (UUID/randomUUID)) "-" ""))
+
+(defn base64-encode
+  "Base64 encode suitable for passing data to the Mixpanel API."
+  ([^String s]
+     (base64-encode s false true))
+  ([^String s chunked url-safe]
+     (String. (Base64/encodeBase64 (.getBytes s) chunked url-safe))))
+
+(defn base64-decode
+  [^String s]
+  (String. (Base64/decodeBase64 (.getBytes s))))
+
+(defn mk-request
+  "event is a string representing the type of event. props should
+   contain the token and distinct_id. m can contain any key values
+   that represent the event's data."
+  [{:keys [distinct-id token] :as props} event m]
+  {:pre [(contains? props :distinct-id)
+         (contains? props :token)]}
+  {:event event
+   :properties (assoc m :distinct_id distinct-id :token token)})
+
+(defn- coerce-bool
+  [b]
+  (if (true? b)
+    "1"
+    "0"))
+
+(defn now
+  []
+  (apply str (drop-last 3 (str (.getTime (Date. ))))))
+
+(defn notify
+  "Sends a notification to the Mixpanel API.
+   token: available in the dashboard
+   event: string representing the type of event
+   m    : a map containing payload information. must contain
+          a distinct-id that identifies the user.
+
+   Example:
+   (let [user-id (generate-uuid)]
+     (notify token \"My Event\" {:distinct-id user-id :key \"value})"
+  [token event {:keys [distinct-id ip test? timestamp] :as payload}]
+  {:pre [(contains? payload :distinct-id)]}
+  (let [props {:token token
+               :distinct-id distinct-id}
+        event-data (assoc (dissoc payload :timestamp :distinct-id) :time (or timestamp (now)))
+        data (mk-request props event event-data)
+        params {:data (-> data (json-str) (base64-encode))
+                :ip (coerce-bool (nil? ip))
+                :test (coerce-bool test?)}]
+    (println "Data: " data)
+    (println "Notifying with: " params)
+    (http/get api-url {:query-params params})))
