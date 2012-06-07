ee0647553706a54a63820952eacdee0c8523a16b
Paul Ingles <paul@oobaloo.co.uk>
2012-06-07 22:47:10 +0100

    add some basic logging; notify returns a future representing the sending of the event

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [clj-http "0.4.2"]
                  [org.clojure/data.json "0.1.2"]
-                 [commons-codec/commons-codec "1.6"]])
+                 [commons-codec/commons-codec "1.6"]
+                 [org.clojure/tools.logging "0.2.3"]]
+  :dev-dependencies [[org.slf4j/slf4j-simple "1.6.4"]])
--- src/clj_mixpanel/core.clj
+++ src/clj_mixpanel/core.clj
@@ -3,7 +3,8 @@
            [java.util Date UUID])
   (:require [clj-http.client :as http]
             [clojure.string :as s])
-  (:use [clojure.data.json :only (json-str)]))
+  (:use [clojure.data.json :only (json-str)]
+        [clojure.tools.logging :only (info)]))
 
 (def api-url "http://api.mixpanel.com/track/")
 
@@ -15,7 +16,7 @@
 (defn base64-encode
   "Base64 encode suitable for passing data to the Mixpanel API."
   ([^String s]
-     (base64-encode s false true))
+     (base64-encode s false false))
   ([^String s chunked url-safe]
      (String. (Base64/encodeBase64 (.getBytes s) chunked url-safe))))
 
@@ -44,7 +45,9 @@
   (apply str (drop-last 3 (str (.getTime (Date. ))))))
 
 (defn notify
-  "Sends a notification to the Mixpanel API.
+  "Sends a notification to the Mixpanel API. Returns a future representing
+   the notification.
+
    token: available in the dashboard
    event: string representing the type of event
    m    : a map containing payload information. must contain
@@ -53,13 +56,15 @@
    Example:
    (let [user-id (generate-uuid)]
      (notify token \"My Event\" {:distinct-id user-id :key \"value})"
-  [token event {:keys [distinct-id ip test? timestamp] :as payload}]
+  [token event {:keys [distinct-id ip test timestamp] :as payload}]
   {:pre [(contains? payload :distinct-id)]}
   (let [props {:token token
                :distinct-id distinct-id}
         event-data (assoc (dissoc payload :timestamp :distinct-id) :time (or timestamp (now)))
         data (mk-request props event event-data)
-        params {:data (-> data (json-str) (base64-encode))
+        params {:data data
                 :ip (coerce-bool (nil? ip))
-                :test (coerce-bool test?)}]
-    (http/get api-url {:query-params params})))
+                :test (coerce-bool test)}
+        query-params (update-in params [:data] #(-> % (json-str) (base64-encode)))]
+    (info params)
+    (future (http/get api-url {:query-params query-params}))))
