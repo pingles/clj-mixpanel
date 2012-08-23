57125eae0b064d53de700287198728dee6c7c5ba
Travis Vachon <travis@copious.com>
2012-08-22 18:20:20 -0700

    Add support for mixpanel People API `set` and `increment`
    Also:
    
     - deprecate `notify` in favor of `track` for parity with the mixpanel
       JavaScript API. `track` and the new functions no longer return a
       future, letting the client add this overhead if desired
    
     - introduce midje and some basic facts
    
     - upgrade project.clj to lein 2

--- test/clj_mixpanel/test/core.clj
+++ test/clj_mixpanel/test/core.clj
@@ -1,6 +1,35 @@
 (ns clj-mixpanel.test.core
+  (:refer-clojure :exclude [set])
   (:use [clj-mixpanel.core])
-  (:use [clojure.test]))
+  (:use [midje.sweet]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+(let [response {:status 200 :body "hi"}
+      token "123"
+      distinct-id "456"
+      event "login"
+      timestamp "12345"]
+
+  (fact (track token event {:distinct-id distinct-id :timestamp timestamp}) => response
+    (provided
+      (post track-url
+       {:data {:event event
+               :properties {:token token :distinct_id distinct-id :time timestamp}}
+        :ip "1" :test "0"})
+      => response))
+
+  (fact (set token distinct-id {:hams :clams}) => response
+    (provided
+      (post engage-url
+            {:data {:$token token
+                    :$distinct_id distinct-id
+                    :$set {:hams :clams}}})
+      => response))
+
+  (fact (increment token distinct-id {:hams :clams}) => response
+    (provided
+      (post engage-url
+            {:data {:$token token
+                    :$distinct_id distinct-id
+                    :$add {:hams :clams}}})
+      => response)))
+
